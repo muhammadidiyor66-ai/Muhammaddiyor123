3ebf4669389660e459a81872e3b1280f6769e869
Muhammaddiyor <Muhammaddiyor.66g@mail.com>
2026-03-27 11:23:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// function totenglash(n, m) {
+//   if (n.length === m.length) {
+//     console.log("true");
+//   } else {
+//     console.log("false");
+//   }
+// }
+// totenglash("uirgc", "uqgnciq");
+// function tosearch(string) {
+//   if (string.length === 0) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// console.log(tosearch(""));
+// function tobolish(n) {
+//   if (n % 5 === 0) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// tobolish(8798740);
+// function tobolish(n) {
+//   if (n % 100 === 0) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// tobolish(8798740);
+// function uzun(s) {
+//   for (let ser = 1; ser < s; s++) {
+//     return ser;
+//   }
+// }
+// console.log(uzun(8798740));
+
